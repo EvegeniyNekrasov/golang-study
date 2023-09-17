a4db79316df53139dfbc8c34e8dc6032e2cff437
Evegeniy Nekrasov <eanekrasovart@gmail.com>
2023-09-17 20:05:58 +0200

    adding go routines

--- cryptomasters/main.go
+++ cryptomasters/main.go
@@ -2,13 +2,28 @@ package main
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/EvegeniyNekrasov/go/cryptomasters/api"
 )
 
 func main() {
-	ans, err := api.GetRate("BTC")
+	currencies := []string{"BTC", "ETH", "BCH"}
+	var wg sync.WaitGroup
+	
+	for _, currency := range currencies {
+		wg.Add(1)
+		go func (currencyCode string) { // immediately invoked function
+			getCurrencyData(currencyCode)
+			wg.Done()
+		}(currency)
+	}
+	wg.Wait()
+}
+
+func getCurrencyData(currency string) {
+	ans, err := api.GetRate(currency)
 	if err == nil {
-		fmt.Printf("The rate fo %v is %.2f", ans.Currency, ans.Price)
+		fmt.Printf("The rate fo %v is %.2f\n", ans.Currency, ans.Price)
 	}
 }
